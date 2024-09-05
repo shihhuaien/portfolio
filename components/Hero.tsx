901116974084fa541ee92d1d0502392aa1a94da9
shihhuaien <timothyshih12032007@gmail.com>
2024-09-05 10:17:05 +0800

    add my working status to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,8 +32,8 @@ const Hero = () => {
             <div className="flex justify-center relative my-20 z-10">
 
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                        The purpose of software development is to serve users
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
+                        I hold a Port of Entry (POE) letter, which allows me to work legally in Canada without requiring an LMIA
                     </p>
 
                     <TextGenerateEffect
