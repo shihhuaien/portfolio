b3e672ff06d595d1621ceb2f5b16a29d7beaa9c7
shihhuaien <timothyshih12032007@gmail.com>
2024-09-05 13:32:57 +0800

    change style of hero section.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
             <div className="flex justify-center relative my-20 z-10">
 
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                         I hold a Port of Entry (POE) letter, which allows me to work legally in Canada without requiring an LMIA
                     </p>
 
